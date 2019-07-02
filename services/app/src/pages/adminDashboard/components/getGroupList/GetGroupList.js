67c0c9e61bd603e9d5a237c2a5c62f0bc4fb79fe
logicboy <faithfulanere@gmail.com>
2019-07-02 12:48:02 +0800

    clean view groups card

--- services/app/src/pages/adminDashboard/components/getGroupList/GetGroupList.js
+++ services/app/src/pages/adminDashboard/components/getGroupList/GetGroupList.js
@@ -6,8 +6,11 @@ import {
   Card,
   CardContent
 } from "@material-ui/core";
+import { FixedSizeList } from "react-window";
 
 import axios from "axios";
+
+import "./GetGroupList.css";
 
 class GroupList extends Component {
   state = {
@@ -21,7 +24,7 @@ class GroupList extends Component {
     axios
       .get(`${this.state.apiUrl}`)
       .then(response => this.setState({ groups: response.data }))
-      .catch(error => this.setState({ errors: error.response.data }));
+      .catch(error => this.setState({ error: error.response.data }));
   };
 
   render() {
@@ -46,12 +49,19 @@ class GroupList extends Component {
             <div>
               {groups.length ? (
                 groups.map(group => (
-                  <ListItem button key={group._id}>
+                  <FixedSizeList
+                    button
+                    key={group._id}
+                    height={400}
+                    width={360}
+                    itemSize={10}
+                    itemCount={200}
+                  >
                     <ListItemText primary={group.name} />
-                  </ListItem>
+                  </FixedSizeList>
                 ))
               ) : (
-                <div>{error}</div>
+                <p class="error">{error}</p>
               )}
             </div>
           </CardContent>
