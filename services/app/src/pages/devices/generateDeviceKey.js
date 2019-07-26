234e9a5475fc58b8149610213aa7472a5c5547a3
logicboy <faithfulanere@gmail.com>
2019-07-27 00:13:29 +0800

    cleanup generate device key

--- services/app/src/pages/devices/generateDeviceKey.js
+++ services/app/src/pages/devices/generateDeviceKey.js
@@ -3,6 +3,8 @@ import { Button, TextField } from "@material-ui/core";
 import axios from "axios";
 import PageTitle from "../../components/PageTitle";
 
+import { Grid, Paper, Card, CardContent } from "@material-ui/core";
+
 class GenerateDeviceKey extends Component {
   state = {
     apiurl: "https://www.terasyshub.io/api/v1/keys",
@@ -18,79 +20,91 @@ class GenerateDeviceKey extends Component {
     });
   };
 
-  onTextChange = e => {
-    const val = e.target.value;
-    this.setState(
-      {
-        [e.target.name]: e.target.value
-      },
-      () => {
-        if (val === "") {
-          this.setState({ key: "" });
-        } else {
-          axios
-            .post(`${this.state.apiurl}/:${this.state.searchText}`)
-            .then(response => this.setState({ key: response.data }))
-            .then(response => console.log(response.data))
-            .then(response => this.setState({ success: response.data }))
-            .catch(error => this.setState({ error: error.response.data }))
-            .catch(err => console.log(err));
-        }
-      }
-    );
-  };
+  // onTextChange = e => {
+  //   const val = e.target.value;
+  //   this.setState(
+  //     {
+  //       [e.target.name]: e.target.value
+  //     },
+  //     () => {
+  //       if (val === "") {
+  //         this.setState({ key: "" });
+  //       } else {
+  //         axios
+  //           .post(`${this.state.apiurl}/:${this.state.searchText}`)
+  //           .then(response => this.setState({ key: response.data }))
+  //           .then(response => console.log(response.data))
+  //           .then(response => this.setState({ success: response.data }))
+  //           .catch(error => this.setState({ error: error.response.data }))
+  //           .catch(err => console.log(err));
+  //       }
+  //     }
+  //   );
+  // };
 
-  //   submitHandler = e => {
-  //     e.preventDefault();
-  //     console.log(this.state);
-  //     axios
-  //       .post(`${this.state.apiurl}`, this.state)
-  //       .then(response => this.setState({ success: response.data }))
-  //       .catch(error => this.setState({ error: error.response.data }));
-  //   };
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post(`${this.state.apiurl}/:${this.state.searchText}`)
+      .then(response => this.setState({ key: response.data }))
+      .then(response => this.setState({ success: response.data }))
+      .catch(error => this.setState({ error: error.response.data }));
+  };
 
   render() {
     const { key, searchText, success, error } = this.state;
 
     return (
-      <div>
+      <React.Fragment>
         <PageTitle title="Generate Key" />
 
-        <TextField
-          name="searchText"
-          value={searchText}
-          onChange={this.onTextChange}
-          fullWidth={true}
-        />
+        <Grid container spacing={8}>
+          <Grid item xs={12} sm={6} xl={3}>
+            <Paper className="paper">
+              <Card>
+                <CardContent>
+                  <form onSubmit={this.submitHandler}>
+                    <TextField
+                      name="searchText"
+                      value={searchText}
+                      onChange={this.changeHandler}
+                      fullWidth={true}
+                      placeholder="Enter Mac Address"
+                    />
 
-        <br />
+                    <br />
+                    <br />
 
-        <form onSubmit={this.submitHandler}>
-          <div>
-            <p>{key}</p>
-          </div>
+                    <Button type="submit" variant="contained" color="primary">
+                      Generate Key
+                    </Button>
+                  </form>
 
-          <br />
+                  <div>
+                    <p>{key}</p>
+                  </div>
 
-          <div>
-            {success ? (
-              <p className="success" variant="h5">
-                {success}
-              </p>
-            ) : (
-              <p className="error" variant="h5">
-                {error}
-              </p>
-            )}
-          </div>
+                  <br />
+                  <br />
 
-          <div>
-            <Button type="submit" variant="contained" color="primary">
-              Generate Key
-            </Button>
-          </div>
-        </form>
-      </div>
+                  <div>
+                    {success ? (
+                      <p className="success" variant="h5">
+                        {success}
+                      </p>
+                    ) : (
+                      <p className="error" variant="h5">
+                        {error}
+                      </p>
+                    )}
+                  </div>
+                </CardContent>
+              </Card>
+            </Paper>
+          </Grid>
+        </Grid>
+      </React.Fragment>
     );
   }
 }
