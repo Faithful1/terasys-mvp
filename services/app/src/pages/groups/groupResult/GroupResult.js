6116208b1cfdfafa51e8633a5f1f66fbf1bd26ac
logicboy <faithfulanere@gmail.com>
2019-07-27 00:13:02 +0800

    remove delete buttong for groups

--- services/app/src/pages/groups/groupResult/GroupResult.js
+++ services/app/src/pages/groups/groupResult/GroupResult.js
@@ -181,11 +181,11 @@ class GroupResult extends Component {
     });
   };
 
-  onDeleteHandler = _id => {
-    axios.delete("/api/v1/groups/:" + _id).then(response => {
-      this._refreshGroups();
-    });
-  };
+  // onDeleteHandler = _id => {
+  //   axios.delete("/api/v1/groups/:" + _id).then(response => {
+  //     this._refreshGroups();
+  //   });
+  // };
 
   /* handle setstate for targets and values */
   changeAddDeviceHandler = e => {
@@ -300,12 +300,6 @@ class GroupResult extends Component {
                       onClick={this.onUserAddHandler.bind(this, group._id)}
                     >
                       Add User
-                    </Button>
-                    <Button
-                      size="small"
-                      onClick={this.onDeleteHandler.bind(this, group._id)}
-                    >
-                      Delete
                     </Button>
                   </CardActions>
                 </Card>
