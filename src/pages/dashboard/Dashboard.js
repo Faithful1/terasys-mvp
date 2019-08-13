c7c62d5dd907eaff44f216bf670b0fcf66b40c79
logicboy <faithfulanere@gmail.com>
2019-08-13 22:04:10 +0800

    populate dropdown with devices

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,192 +1,209 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import axios from "axios";
+
+import { Grid } from "@material-ui/core";
+import { ResponsiveContainer } from "recharts";
 import {
-  Grid,
+  TextField,
+  CircularProgress,
+  Button,
   Select,
-  OutlinedInput,
+  FormHelperText,
   MenuItem,
-  withStyles
+  FormControl,
+  InputLabel
 } from "@material-ui/core";
 
-import DeviceStat from "./components/DeviceStat/DeviceStat";
 import Widget from "../../components/Widget";
 import PageTitle from "../../components/PageTitle";
 import { Typography } from "../../components/Wrappers";
-import Dot from "../../components/Sidebar/components/Dot";
 
-const Dashboard = ({ classes, theme, ...props }) => {
-  return (
-    <React.Fragment>
-      <PageTitle title="Dashboard" />
-      <Grid container spacing={32}>
-        <Grid item xs={12}>
-          <Widget
-            bodyClass={classes.mainChartBody}
-            header={
-              <div className={classes.mainChartHeader}>
-                <Typography variant="headline" color="textSecondary">
-                  View Live Data
-                </Typography>
-                <div className={classes.mainChartHeaderLabels}>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="warning" />
-                    <Typography className={classes.mainChartLegentElement}>
-                      Tablet
-                    </Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>
-                      Mobile
-                    </Typography>
-                  </div>
-                  <div className={classes.mainChartHeaderLabel}>
-                    <Dot color="primary" />
-                    <Typography className={classes.mainChartLegentElement}>
-                      Desktop
-                    </Typography>
-                  </div>
-                </div>
-                <Select
-                  value={props.mainChartState}
-                  onChange={e => props.setMainChartState(e.target.value)}
-                  input={
-                    <OutlinedInput
-                      labelWidth={0}
-                      classes={{
-                        notchedOutline: classes.mainChartSelectRoot,
-                        input: classes.mainChartSelect
-                      }}
-                    />
-                  }
-                  autoWidth
-                >
-                  <MenuItem value="daily">Daily</MenuItem>
-                  <MenuItem value="weekly">Weekly</MenuItem>
-                  <MenuItem value="monthly">Monthly</MenuItem>
-                </Select>
-              </div>
-            }
-          >
-            <DeviceStat />
-          </Widget>
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
-};
+class Dashboard extends Component {
+  state = {
+    getApiUrl: "https://www.terasyshub.io/api/v1/devices/",
+    devices: [],
 
-const styles = theme => ({
-  card: {
-    minHeight: "100%",
-    display: "flex",
-    flexDirection: "column"
-  },
-  visitsNumberContainer: {
-    display: "flex",
-    alignItems: "center",
-    flexGrow: 1,
-    paddingBottom: theme.spacing.unit
-  },
-  progressSection: {
-    marginBottom: theme.spacing.unit
-  },
-  progressTitle: {
-    marginBottom: theme.spacing.unit * 2
-  },
-  progress: {
-    marginBottom: theme.spacing.unit,
-    backgroundColor: theme.palette.primary.main
-  },
-  pieChartLegendWrapper: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "flex-end",
-    marginRight: theme.spacing.unit
-  },
-  legendItemContainer: {
-    display: "flex",
-    alignItems: "center",
-    marginBottom: theme.spacing.unit
-  },
-  fullHeightBody: {
-    display: "flex",
-    flexGrow: 1,
-    flexDirection: "column",
-    justifyContent: "space-between"
-  },
-  tableWidget: {
-    overflowX: "auto"
-  },
-  progressBar: {
-    backgroundColor: theme.palette.warning.main
-  },
-  performanceLegendWrapper: {
-    display: "flex",
-    flexGrow: 1,
-    alignItems: "center",
-    marginBottom: theme.spacing.unit
-  },
-  legendElement: {
-    display: "flex",
-    alignItems: "center",
-    marginRight: theme.spacing.unit * 2
-  },
-  legendElementText: {
-    marginLeft: theme.spacing.unit
-  },
-  serverOverviewElement: {
-    display: "flex",
-    alignItems: "center",
-    maxWidth: "100%"
-  },
-  serverOverviewElementText: {
-    minWidth: 145,
-    paddingRight: theme.spacing.unit * 2
-  },
-  serverOverviewElementChartWrapper: {
-    width: "100%"
-  },
-  mainChartBody: {
-    overflowX: "auto"
-  },
-  mainChartHeader: {
-    width: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    [theme.breakpoints.only("xs")]: {
-      flexWrap: "wrap"
-    }
-  },
-  mainChartHeaderLabels: {
-    display: "flex",
-    alignItems: "center",
-    [theme.breakpoints.only("xs")]: {
-      order: 3,
-      width: "100%",
-      justifyContent: "center",
-      marginTop: theme.spacing.unit * 3,
-      marginBottom: theme.spacing.unit * 2
-    }
-  },
-  mainChartHeaderLabel: {
-    display: "flex",
-    alignItems: "center",
-    marginLeft: theme.spacing.unit * 3
-  },
-  mainChartSelectRoot: {
-    borderColor: theme.palette.text.hint + "80 !important"
-  },
-  mainChartSelect: {
-    padding: 10,
-    paddingRight: 25
-  },
-  mainChartLegentElement: {
-    fontSize: "18px !important",
-    marginLeft: theme.spacing.unit
+    deviceData: [],
+    isLoading: false,
+    error: "",
+    metricsChoice: "",
+    macAddress: ""
+  };
+
+  componentDidMount() {
+    this._getDevices();
   }
-});
 
-export default withStyles(styles, { withTheme: true })(Dashboard);
+  submitHandler = e => {
+    e.preventDefault();
+
+    const { metricsChoice, macAddress } = this.state;
+
+    this.setState({
+      isLoading: true
+    });
+
+    const headers = {
+      "Content-Type": "application/json"
+    };
+
+    axios
+      .get(
+        `https://www.terasyshub.io/api/v1/data/${metricsChoice}/${macAddress}`,
+        { headers: headers }
+      )
+      .then(response =>
+        this.setState({
+          deviceData: response.data,
+          isLoading: false,
+          metricsChoice: "",
+          macAddress: ""
+        })
+      )
+      .catch(error =>
+        this.setState({
+          error: error.response.data,
+          isLoading: false
+        })
+      );
+  };
+
+  onChangeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  _getDevices() {
+    axios
+      .get(`${this.state.getApiUrl}`)
+      .then(response => this.setState({ devices: response.data }))
+      .catch(error => this.setState({ error: error.response.data }));
+  }
+
+  render() {
+    const {
+      devices,
+      deviceData,
+      macAddress,
+      error,
+      metricsChoice,
+      isLoading
+    } = this.state;
+
+    return (
+      <React.Fragment>
+        <PageTitle title="Dashboard" />
+        <Grid container spacing={32}>
+          <Grid item xs={12}>
+            <Widget
+              header={
+                <div>
+                  <Typography variant="h5" color="textSecondary">
+                    Search
+                  </Typography>
+                </div>
+              }
+            >
+              <ResponsiveContainer width="100%" minWidth={500} height={250}>
+                <form onSubmit={this.submitHandler}>
+                  {error ? (
+                    <Typography color="secondary" className="errors">
+                      {error}
+                    </Typography>
+                  ) : null}
+
+                  <FormControl required>
+                    <InputLabel htmlFor="metric-native-required">
+                      Metric
+                    </InputLabel>
+                    <Select
+                      native
+                      name="macAddress"
+                      value={macAddress}
+                      onChange={this.onChangeHandler}
+                      margin="dense"
+                      inputProps={{
+                        id: "metric-native-required"
+                      }}
+                    >
+                      <option value="" />
+                      <option value="temperature">temperature</option>
+                      <option value="humidity">humidity</option>
+                    </Select>
+                    <FormHelperText>Select Metrics</FormHelperText>
+                  </FormControl>
+
+                  <br />
+                  <br />
+
+                  <FormControl required>
+                    <InputLabel htmlFor="metric-native-required">
+                      Metric
+                    </InputLabel>
+                    {devices.map(device => (
+                      <Select
+                        native
+                        key={device._id}
+                        name="metricsChoice"
+                        value={metricsChoice}
+                        onChange={this.onChangeHandler}
+                        margin="dense"
+                        inputProps={{
+                          id: "metric-native-required"
+                        }}
+                      >
+                        <option value="" />
+                        <option value={device.name}>{device.name}</option>
+                      </Select>
+                    ))}
+                    <FormHelperText>Select Metrics</FormHelperText>
+                  </FormControl>
+
+                  <br />
+                  <br />
+
+                  <div className="creatingButtonContainer">
+                    {isLoading ? (
+                      <CircularProgress size={26} />
+                    ) : (
+                      <Button
+                        type="submit"
+                        size="large"
+                        variant="contained"
+                        color="primary"
+                      >
+                        Search Device
+                      </Button>
+                    )}
+                  </div>
+                </form>
+              </ResponsiveContainer>
+            </Widget>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Widget
+              header={
+                <div>
+                  <Typography variant="h5" color="textSecondary">
+                    Live Data From Device
+                  </Typography>
+                </div>
+              }
+            >
+              <ResponsiveContainer width="100%" minWidth={500} height={500}>
+                <Typography variant="h5" color="textSecondary">
+                  Device
+                </Typography>
+              </ResponsiveContainer>
+            </Widget>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Dashboard;
--- src/pages/dashboard/components/DeviceStat/DeviceStat.js
+++ src/pages/dashboard/components/DeviceStat/DeviceStat.js
@@ -1,4 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
+
+import {
+  Grid,
+  Select,
+  OutlinedInput,
+  MenuItem,
+  withStyles
+} from "@material-ui/core";
 import {
   ResponsiveContainer,
   LineChart,
@@ -9,6 +17,10 @@ import {
   Tooltip,
   Legend
 } from "recharts";
+
+import Widget from "../../../../components/Widget";
+import { Typography } from "../../../../components/Wrappers";
+import Dot from "../../../../components/Sidebar/components/Dot";
 
 const data = [
   {
@@ -55,37 +67,202 @@ const data = [
   }
 ];
 
-class DeviceStat extends Component {
-  render() {
-    return (
-      <ResponsiveContainer width="100%" minWidth={500} height={350}>
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5
-          }}
+const DeviceStat = ({ classes, theme, ...props }) => {
+  return (
+    <Grid container spacing={32}>
+      <Grid item xs={12}>
+        <Widget
+          bodyClass={classes.mainChartBody}
+          header={
+            <div className={classes.mainChartHeader}>
+              <Typography variant="headline" color="textSecondary">
+                View Live Data
+              </Typography>
+              <div className={classes.mainChartHeaderLabels}>
+                <div className={classes.mainChartHeaderLabel}>
+                  <Dot color="warning" />
+                  <Typography className={classes.mainChartLegentElement}>
+                    Tablet
+                  </Typography>
+                </div>
+                <div className={classes.mainChartHeaderLabel}>
+                  <Dot color="primary" />
+                  <Typography className={classes.mainChartLegentElement}>
+                    Mobile
+                  </Typography>
+                </div>
+                <div className={classes.mainChartHeaderLabel}>
+                  <Dot color="primary" />
+                  <Typography className={classes.mainChartLegentElement}>
+                    Desktop
+                  </Typography>
+                </div>
+              </div>
+              <Select
+                value={props.mainChartState}
+                onChange={e => props.setMainChartState(e.target.value)}
+                input={
+                  <OutlinedInput
+                    labelWidth={0}
+                    classes={{
+                      notchedOutline: classes.mainChartSelectRoot,
+                      input: classes.mainChartSelect
+                    }}
+                  />
+                }
+                autoWidth
+              >
+                <MenuItem value="daily">Daily</MenuItem>
+                <MenuItem value="weekly">Weekly</MenuItem>
+                <MenuItem value="monthly">Monthly</MenuItem>
+              </Select>
+            </div>
+          }
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="pv"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-    );
-  }
-}
+          <ResponsiveContainer width="100%" minWidth={500} height={350}>
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        </Widget>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default DeviceStat;
+const styles = theme => ({
+  card: {
+    minHeight: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  visitsNumberContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexGrow: 1,
+    paddingBottom: theme.spacing.unit
+  },
+  progressSection: {
+    marginBottom: theme.spacing.unit
+  },
+  progressTitle: {
+    marginBottom: theme.spacing.unit * 2
+  },
+  progress: {
+    marginBottom: theme.spacing.unit,
+    backgroundColor: theme.palette.primary.main
+  },
+  pieChartLegendWrapper: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    marginRight: theme.spacing.unit
+  },
+  legendItemContainer: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: theme.spacing.unit
+  },
+  fullHeightBody: {
+    display: "flex",
+    flexGrow: 1,
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
+  tableWidget: {
+    overflowX: "auto"
+  },
+  progressBar: {
+    backgroundColor: theme.palette.warning.main
+  },
+  performanceLegendWrapper: {
+    display: "flex",
+    flexGrow: 1,
+    alignItems: "center",
+    marginBottom: theme.spacing.unit
+  },
+  legendElement: {
+    display: "flex",
+    alignItems: "center",
+    marginRight: theme.spacing.unit * 2
+  },
+  legendElementText: {
+    marginLeft: theme.spacing.unit
+  },
+  serverOverviewElement: {
+    display: "flex",
+    alignItems: "center",
+    maxWidth: "100%"
+  },
+  serverOverviewElementText: {
+    minWidth: 145,
+    paddingRight: theme.spacing.unit * 2
+  },
+  serverOverviewElementChartWrapper: {
+    width: "100%"
+  },
+  mainChartBody: {
+    overflowX: "auto"
+  },
+  mainChartHeader: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    [theme.breakpoints.only("xs")]: {
+      flexWrap: "wrap"
+    }
+  },
+  mainChartHeaderLabels: {
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.only("xs")]: {
+      order: 3,
+      width: "100%",
+      justifyContent: "center",
+      marginTop: theme.spacing.unit * 3,
+      marginBottom: theme.spacing.unit * 2
+    }
+  },
+  mainChartHeaderLabel: {
+    display: "flex",
+    alignItems: "center",
+    marginLeft: theme.spacing.unit * 3
+  },
+  mainChartSelectRoot: {
+    borderColor: theme.palette.text.hint + "80 !important"
+  },
+  mainChartSelect: {
+    padding: 10,
+    paddingRight: 25
+  },
+  mainChartLegentElement: {
+    fontSize: "18px !important",
+    marginLeft: theme.spacing.unit
+  }
+});
+
+export default withStyles(styles, { withTheme: true })(DeviceStat);
