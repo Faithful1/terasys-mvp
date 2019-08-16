2f59dbf14e8b066527705007050d60eaaef6f5e7
logicboy <faithfulanere@gmail.com>
2019-08-16 23:01:44 +0800

    data on graph works

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -19,147 +19,13 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
-  Brush,
-  AreaChart,
-  Area
+  Legend
 } from "recharts";
 import Widget from "../../components/Widget";
 import PageTitle from "../../components/PageTitle";
 import { Typography } from "../../components/Wrappers";
+import DeviceStat from "./components/DeviceStat/DeviceStat";
 
-const data = [
-  {
-    _id: "59cb88a08b7bad4aae8bd5d5",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 28.3,
-    unit: "C",
-    timestamp: 1506510995,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb7bd38b7bad4aae8bd5bd",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.9,
-    unit: "C",
-    timestamp: 1506507720,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb7b908b7bad4aae8bd5bb",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.9,
-    unit: "C",
-    timestamp: 1506507660,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb7b548b7bad4aae8bd5b9",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.9,
-    unit: "C",
-    timestamp: 1506507600,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb7b178b7bad4aae8bd5b5",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.8,
-    unit: "C",
-    timestamp: 1506507540,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb7adb8b7bad4aae8bd5b3",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.8,
-    unit: "C",
-    timestamp: 1506507480,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb7ab68b7bad4aae8bd5b1",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.8,
-    unit: "C",
-    timestamp: 1506507420,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb7a2b8b7bad4aae8bd5af",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.8,
-    unit: "C",
-    timestamp: 1506507300,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb79ef8b7bad4aae8bd5ab",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.8,
-    unit: "C",
-    timestamp: 1506507240,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  },
-  {
-    _id: "59cb78838b7bad4aae8bd5a4",
-    mac: "5c:cf:7f:c4:10:36",
-    value: 27.5,
-    unit: "C",
-    timestamp: 1506506880,
-    type: "temperature",
-    __v: 0,
-    location: {
-      lon: 3.38236,
-      lat: 6.497492
-    }
-  }
-];
 class Dashboard extends Component {
   state = {
     isLoading: false,
@@ -220,8 +86,6 @@ class Dashboard extends Component {
   }
 
   render() {
-    let viewDeviceDataContent;
-
     const {
       deviceName,
       devices,
@@ -236,41 +100,6 @@ class Dashboard extends Component {
         {device.name}
       </option>
     ));
-
-    if (deviceViewData) {
-      viewDeviceDataContent = (
-        <ResponsiveContainer width="100%" minWidth={500} height={500}>
-          <Typography variant="h5" color="textSecondary">
-            <LineChart
-              width={500}
-              height={300}
-              data={deviceViewData}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="timestamp" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="value"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-          </Typography>
-        </ResponsiveContainer>
-      );
-    } else {
-      viewDeviceDataContent = null;
-    }
 
     return (
       <React.Fragment>
@@ -376,17 +205,7 @@ class Dashboard extends Component {
           </Grid>
 
           <Grid item xs={12}>
-            <Widget
-              header={
-                <div>
-                  <Typography variant="h5" color="textSecondary">
-                    Live Data From Device
-                  </Typography>
-                </div>
-              }
-            >
-              {viewDeviceDataContent}
-            </Widget>
+            <DeviceStat Content={deviceViewData} />
           </Grid>
         </Grid>
       </React.Fragment>
--- src/pages/dashboard/components/DeviceStat/DeviceStat.js
+++ src/pages/dashboard/components/DeviceStat/DeviceStat.js
@@ -22,51 +22,6 @@ import Widget from "../../../../components/Widget";
 import { Typography } from "../../../../components/Wrappers";
 import Dot from "../../../../components/Sidebar/components/Dot";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
-
 const DeviceStat = ({ classes, theme, ...props }) => {
   return (
     <Grid container spacing={32}>
@@ -98,24 +53,6 @@ const DeviceStat = ({ classes, theme, ...props }) => {
                   </Typography>
                 </div>
               </div>
-              <Select
-                value={props.mainChartState}
-                onChange={e => props.setMainChartState(e.target.value)}
-                input={
-                  <OutlinedInput
-                    labelWidth={0}
-                    classes={{
-                      notchedOutline: classes.mainChartSelectRoot,
-                      input: classes.mainChartSelect
-                    }}
-                  />
-                }
-                autoWidth
-              >
-                <MenuItem value="daily">Daily</MenuItem>
-                <MenuItem value="weekly">Weekly</MenuItem>
-                <MenuItem value="monthly">Monthly</MenuItem>
-              </Select>
             </div>
           }
         >
@@ -123,7 +60,7 @@ const DeviceStat = ({ classes, theme, ...props }) => {
             <LineChart
               width={500}
               height={300}
-              data={data}
+              data={props.Content}
               margin={{
                 top: 5,
                 right: 30,
@@ -132,13 +69,13 @@ const DeviceStat = ({ classes, theme, ...props }) => {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="timestamp" />
               <YAxis />
               <Tooltip />
               <Legend />
               <Line
                 type="monotone"
-                dataKey="pv"
+                dataKey="value"
                 stroke="#8884d8"
                 activeDot={{ r: 8 }}
               />
